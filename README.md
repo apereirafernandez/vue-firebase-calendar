29f94c0d9440c777f1ceb1908f6c352577a69deb
Adrian Pereira <apereirafernandez@gmail.com>
2020-08-10 19:06:07 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,24 +1,48 @@
 # vue-calendar
 
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### Setting Up Firebase
+
+1. Set up a new project in Firebase with Database enabled.
+2. Implement the following Firebase SDK scripts in main.js:
+
+```import firebase from "firebase";
+firebase.initializeApp({
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+});
+
+export const db = firebase.firestore();
+```
--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
+import VueTextAreaAutosize from 'vue-textarea-autosize';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+Vue.use(VueTextAreaAutosize);
 
 Vue.config.productionTip = false
 
+firebase.initializeApp({
+  apiKey: "AIzaSyDNi8Nhd5nqg4fQk_-irBEzHdHbyjnuaj0",
+  authDomain: "vue-calendar-35533.firebaseapp.com",
+  databaseURL: "https://vue-calendar-35533.firebaseio.com",
+  projectId: "vue-calendar-35533",
+  storageBucket: "vue-calendar-35533.appspot.com",
+  messagingSenderId: "1039593377584",
+  appId: "1:1039593377584:web:c55d937c5b9e11375542f5"
+
+});
+
+
+export const db = firebase.firestore();
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
